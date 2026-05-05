2d2a2007487cde551000944574ae51355311f0c8
hariharan-16 <harioz1515@gmail.com>
2026-05-05 12:56:50 +0530

    Base class codes added

---
+++ src/test/java/base/BaseClass.java
@@ -0,0 +1,54 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utilities.Utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+public class BaseClass {
+    public WebDriver driver;
+    public Properties prop;
+    FileInputStream file;
+
+    public BaseClass() throws IOException {
+        prop = new Properties();
+        file = new FileInputStream("src/test/resources/testData.prop");
+        prop.load(file);
+    }
+
+    @BeforeMethod
+    public void setUp() throws IOException {
+        driver = initializeBrowser(prop.getProperty("browser"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Utilities.IMPLICIT_WAIT));
+        driver.get(prop.getProperty("url"));
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        if(driver != null){
+            driver.quit();
+        }
+    }
+
+    public WebDriver initializeBrowser(String browser){
+        WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
+
+        if(browser.equals("chrome")){
+            driver = WebDriverManager.chromedriver().create();
+        } else if(browser.equals("firefox")){
+            driver = WebDriverManager.firefoxdriver().create();
+        }
+
+        return driver;
+    }
+
+}
