a416dc7546d1376d887aeb0adac2c0a7dbc87513
hariharan-16 <harioz1515@gmail.com>
2026-05-05 13:36:53 +0530

    Home Page POM file updated

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+    WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(linkText = "Home")
+    private WebElement homeButton;
+
+    @FindBy(linkText = "Signup / Login")
+    private WebElement loginOrSignupButton;
+
+    public void clickHomeButton() {
+        homeButton.click();
+    }
+
+    public void clickLoginOrSignupButton() {
+        loginOrSignupButton.click();
+    }
+}
