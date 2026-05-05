0bfabe494a476542de0bc83c72a0c4ddd0cc97e5
hariharan-16 <harioz1515@gmail.com>
2026-05-05 16:27:39 +0530

    Excel data getter method added in Utilities file

--- src/test/java/utilities/Utilities.java
+++ src/test/java/utilities/Utilities.java
@@ -1,5 +1,36 @@
 package utilities;
 
-public class Utilities {
+import base.BaseClass;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+
+public class Utilities extends BaseClass {
+
+    public Utilities() throws Exception {
+        super();
+    }
+
     public static final int IMPLICIT_WAIT = 10;
+
+    public static String getExcelData(String sheetName, int rowNum, int cellNum) {
+
+        XSSFSheet sheet = wb.getSheet(sheetName);
+        XSSFRow row = sheet.getRow(rowNum);
+        XSSFCell cell = row.getCell(cellNum);
+        CellType cellType = cell.getCellType();
+        String data = "";
+
+        switch (cellType) {
+            case STRING:
+                data = cell.getStringCellValue();
+                break;
+            case NUMERIC:
+                data = Integer.toString((int)cell.getNumericCellValue());
+                break;
+        }
+
+        return data;
+    }
 }
