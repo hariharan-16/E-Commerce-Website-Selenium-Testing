d4d7c377936146ce7670c4aab58a6a7e8d3f0604
hariharan-16 <harioz1515@gmail.com>
2026-05-05 11:58:47 +0530

    Test Connection File added

---
+++ src/main/java/TestConnection.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestConnection {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
