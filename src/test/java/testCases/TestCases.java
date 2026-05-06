99acd854120925fa060d2ce9c9860cea797259d8
hariharan-16 <harioz1515@gmail.com>
2026-05-06 15:26:15 +0530

    Assertion's added to the test cases

--- src/test/java/testCases/TestCases.java
+++ src/test/java/testCases/TestCases.java
@@ -1,6 +1,9 @@
 package testCases;
 
 import base.BaseClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.Utilities;
 
@@ -14,11 +17,15 @@ public class TestCases extends BaseClass {
     public void testCase1() throws InterruptedException {
         // Register User
         hp.clickLoginOrSignupButton();
+        WebElement signUpText = driver.findElement(By.xpath("//h2[text()='New User Signup!']"));
+        Assert.assertTrue(signUpText.isDisplayed(), "Sign Up text is not displayed");
 
         sp.enterName(Utilities.getExcelData("Sheet1", 0, 0));
         sp.enterSignupEmail(Utilities.getExcelData("Sheet1", 1, 0));
         Thread.sleep(5000);
         sp.clickSignUpButton();
+        WebElement accountInfoText = driver.findElement(By.xpath("//b[text()='Enter Account Information']"));
+        Assert.assertTrue(accountInfoText.isDisplayed(), "Account Information text is not displayed");
 
         sp.selectTitleMr();
         sp.enterPassword(Utilities.getExcelData("Sheet1", 2, 0));
@@ -33,12 +40,16 @@ public class TestCases extends BaseClass {
         sp.enterMobileNumber(Utilities.getExcelData("Sheet1", 9, 0));
         Thread.sleep(5000);
         sp.clickCreateAccountButton();
+        WebElement accountCreatedText = driver.findElement(By.xpath("//b[text()='Account Created!']"));
+        Assert.assertTrue(accountCreatedText.isDisplayed(), "Account Created text is not displayed");
 
         Thread.sleep(5000);
         sp.clickContinueButton();
 
         Thread.sleep(5000);
         sp.clickDeleteAccountButton();
+        WebElement accountDeletedText = driver.findElement(By.xpath("//b[text()='Account Deleted!']"));
+        Assert.assertTrue(accountDeletedText.isDisplayed(), "Account Deleted text is not displayed");
 
         Thread.sleep(5000);
         sp.clickContinueButton();
